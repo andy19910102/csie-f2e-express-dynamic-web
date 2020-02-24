b764ff6fc8bb459fc1bc2eb13837c827538158b9
andy19910102 <andy19910102@gmail.com>
2020-02-24 16:26:27 +0800

    Proj init

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+// 首頁路由
+router.get('/', async function (req, res, next) {
+  // TODO: 取得產品列表
+  res.render('index');
+});
+
+// API示範頁面
+router.get('/api-demo', function (req, res, next) {
+  res.render('api-demo');
+});
+
+module.exports = router;
