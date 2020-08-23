9a65aaf4e58a0530ed08ac98177d45ae59cc1fff
andy19910102 <andy19910102@gmail.com>
2020-08-23 08:21:27 +0800

    Add loop example on index.ejs

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // 首頁路由
 router.get('/', function (req, res, next) {
   // TODO: 取得產品列表
+
+  // 將產品列表傳遞到模板
+  res.locals.productList = [];
   res.render('index');
 });
 
